1c7b94dbe56bd8b860fff1e1014033bc4b81a758
Ehab AlBadawy <ealbadawy@albany.edu>
2018-05-11 00:01:19 -0400

    add get_best_epoch fn
    get the hights ccc value based on dev set using tensorboard logs

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,8 @@
 import argparse
-
+import os
+# import tensorflow as tf
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+import numpy as np
 
 def get_args():
     argparser = argparse.ArgumentParser(description=__doc__)
@@ -10,3 +13,17 @@ def get_args():
         help='The Configuration file')
     args = argparser.parse_args()
     return args
+
+def my_mkdir(d):
+    if not os.path.exists(d):
+        os.mkdir(d)
+        return 1
+    return 0
+
+def get_best_epoch(config):
+    exp_name = config.init_exp
+    x = EventAccumulator(path='../experiments/%s/summary/dev'%exp_name)
+    x.Reload()
+    _, _, vals = zip(*x.Scalars('ccc_%s_1'%config.target_ccc))
+
+    return np.argmax(list(vals))+1
