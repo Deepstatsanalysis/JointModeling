4af2d75b89dfe44cb0dae94321b8d5875dc61956
Ehab AlBadawy <ealbadawy@albany.edu>
2018-05-11 00:27:53 -0400

    update save/load fn (BaseModel)

--- base/base_model.py
+++ base/base_model.py
@@ -11,19 +11,17 @@ class BaseModel:
         # init the epoch counter
         self.init_cur_epoch()
 
-    # save function that saves the checkpoint in the path defined in the config file
-    def save(self, sess):
+    # save function thet save the checkpoint in the path defined in 'outdir' var
+    def save(self, sess, outdir):
         print("Saving model...")
-        self.saver.save(sess, self.config.checkpoint_dir, self.global_step_tensor)
+        self.saver.save(sess, outdir, self.global_step_tensor)
         print("Model saved")
 
-    # load latest checkpoint from the experiment path defined in the config file
-    def load(self, sess):
-        latest_checkpoint = tf.train.latest_checkpoint(self.config.checkpoint_dir)
-        if latest_checkpoint:
-            print("Loading model checkpoint {} ...\n".format(latest_checkpoint))
-            self.saver.restore(sess, latest_checkpoint)
-            print("Model loaded")
+    # load checkpoint from the experiment path defined in 'model' var
+    def load(self, sess, model):
+        print("Loading model checkpoint {} ...\n".format(model))
+        self.saver.restore(sess, model)
+        print("Model loaded")
 
     # just initialize a tensorflow variable to use it as epoch counter
     def init_cur_epoch(self):
