5577f7db0a2aa4ee51ac1857c1cad41e3b5f4523
Ehab AlBadawy <ealbadawy@albany.edu>
2018-05-11 00:58:38 -0400

    fix global_step_tensor bug

--- base/base_model.py
+++ base/base_model.py
@@ -29,11 +29,11 @@ class BaseModel:
             self.cur_epoch_tensor = tf.Variable(0, trainable=False, name='cur_epoch')
             self.increment_cur_epoch_tensor = tf.assign(self.cur_epoch_tensor, self.cur_epoch_tensor + 1)
 
-    # just initialize a tensorflow variable to use it as global step counter
-    def init_global_step(self):
+    # just inialize a tensorflow variable to use it as global step counter
+    def init_global_step(self, init_value):
         # DON'T forget to add the global step tensor to the tensorflow trainer
         with tf.variable_scope('global_step'):
-            self.global_step_tensor = tf.Variable(0, trainable=False, name='global_step')
+            self.global_step_tensor = tf.Variable(init_value, trainable=False, name='global_step')
 
     def init_saver(self):
         # just copy the following line in your child class
