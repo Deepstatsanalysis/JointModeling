3c632e1f8037a260622206d28c45f0ef5f2273e6
Ehab AlBadawy <ealbadawy@albany.edu>
2018-05-10 23:18:45 -0400

    init BLSTM model

---
+++ models/BLSTM.py
@@ -0,0 +1,12 @@
+from base.base_model import BaseModel
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import rnn
+from tensorflow.contrib.layers import flatten
+
+class BLSTM(BaseModel):
+    def __init__(self, config, data):
+        super(BLSTM, self).__init__(config, data)
+        self.build_model()
+        self.init_saver()
