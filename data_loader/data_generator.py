9f7d60ec0d771524fc8405c7a90953ef915cd374
Ehab AlBadawy <ealbadawy@albany.edu>
2018-05-10 23:14:06 -0400

    creat load_combined_gt fn
    load the continuous GT for all speakers in a given subset/dim

--- data_loader/data_generator.py
+++ data_loader/data_generator.py
@@ -38,6 +38,25 @@ class DataGenerator:
         gt = np.array(gt)
         return gt.reshape(gt.shape[0],1)
 
+    def load_combined_gt(self, dim, subsets):
+        """ Loads GT for a combined subsets
+        Args:
+            dim: GT dimension, take values of 'arousal', or 'valence'
+            subsets: list of the returend GT subsets, i.e. ['train'] or ['train', 'dev']
+
+        Returns:
+            numpy array of size (67500, 1) for the requested GT based on the args
+        """
+        
+        gt = []
+        
+        for subset in subsets:
+            for idx in range(1,10):
+                gt.append(self.get_gt_cont(dim, subset, idx))
+               
+        gt = np.array(gt).flatten()
+        return gt.reshape(gt.shape[0],1)
+        
     def next_batch(self, batch_size):
         idx = np.random.choice(500, batch_size)
         yield self.input[idx], self.y[idx]
